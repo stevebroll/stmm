0a82513149b2d2649b9457507f462b7004012792
stevebroll <stevebroll@github.com>
2018-07-16 14:26:56 -0400

    change variable name from LR to run2 to follow style guide

--- rfunctions/single_subject/lm.R
+++ rfunctions/single_subject/lm.R
@@ -6,34 +6,36 @@ source('/share/RDirectory/stmm/rfunctions/single_subject/preprocessing.R')
 # Fit lm() for Single Coord ---------------------------------------------------
 
 # use first row of mapped measurements as y, task as x
-LR_lm_fit <- lm(LR_vinf[1, ] ~ design_2)
+run2_lm_fit <- lm(run2_vinf[1, ] ~ design2)
 
 # Fit lm() for Each Coord -----------------------------------------------------
 
 # define empty vars for slopes, p-values, and std. error
 coefs <- c()
 pvals <- c()
-error <- c()
+stder <- c()
 # create progress bar for loop
-pb = txtProgressBar(min = 0, max = dim(LR_vinf)[1], initial = 0, style = 3) 
+pb = txtProgressBar(min = 0, max = dim(run2_vinf)[1], initial = 0, style = 3) 
 # loop through all coords, with i being coord number
-for (i in 1:dim(LR_vinf)[1]) {
-  if (!any(is.na(LR_vinf[i, ]))) {
-    sumfit   <- summary(lm(LR_vinf[i, ] ~ design_2))
+for (i in 1:dim(run2_vinf)[1]) {
+  if (!any(is.na(run2_vinf[i, ]))) {
+    sumfit   <- summary(lm(run2_vinf[i, ] ~ design2))
     coefs[i] <- sumfit$coefficients[2,1]
-    error[i] <- sumfit$coefficients[2,2]
+    stder[i] <- sumfit$coefficients[2,2]
     pvals[i] <- sumfit$coefficients[2,4]
   } else {
     coefs[i] <- NA
-    error[i] <- NA
+    stder[i] <- NA
     pvals[i] <- NA
   }
   setTxtProgressBar(pb,i)
 }
 
+summary(coefs)
+summary(stder)
+summary(pvals)
 
 # NOTES -----------------------------------------------------------------------
 # need non-local file path
-# correct to bind LR_L and LR_R?
 # lm fit p-vals and coef assignments will only work for single task
 # should we use na.exlude as na.action in lm?
